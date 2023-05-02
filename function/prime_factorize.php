9c7155e8779e31696f79ed22099be24d92a4663e
noko206 <61347127+noko206@users.noreply.github.com>
2023-05-02 14:20:18 +0900

    add: prime_factorize

---
+++ function/prime_factorize.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * 素因数分解
+ * 計算量：O(√n)
+ * @param int 素因数分解したい値
+ * @return array [素因数,指数]となる配列
+ */
+function prime_factorize(int $n): array
+{
+	$a = [];
+	for ($i = 2; $i * $i <= $n; ++$i) {
+		if ($n % $i !== 0) continue;
+		$ex = 0;
+		while ($n % $i === 0) {
+			++$ex;
+			$n = intdiv($n, $i);
+		}
+		$a[] = [$i, $ex];
+	}
+	if ($n !== 1) {
+		$a[] = [$n, 1];
+	}
+	return $a;
+}
