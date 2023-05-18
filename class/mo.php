f91cd172df5d896bed58ced9a4e0634bc7ff287c
noko206 <61347127+noko206@users.noreply.github.com>
2023-05-18 23:04:04 +0900

    docs: Mo's Algorithmのコメントを追加

--- class/mo.php
+++ class/mo.php
@@ -1,20 +1,49 @@
 <?php
 
+/**
+ * Mo's Algorithm
+ * 区間[0,n)に対するq個の区間[l,r)に関するクエリをO(n√q)で処理できる
+ */
 class Mo
 {
+	/** @var int 区間の長さ */
 	private $n;
+	/** @var array クエリの区間[l,r)の配列 */
 	private $lr;
 
+	/**
+	 * プロパティの初期化
+	 * 計算量：O(1)
+	 * @param int $n 区間の長さ
+	 */
 	public function __construct(int $n)
 	{
 		$this->n = $n;
+		$this->lr = [];
 	}
 
+	/**
+	 * クエリの追加｜[l,r) 0-indexed
+	 * 計算量：O(1)
+	 * @param int $l
+	 * @param int $r
+	 * @return void
+	 */
 	public function add(int $l, int $r): void
 	{
 		$this->lr[] = [$l, $r];
 	}
 
+	/**
+	 * ビルド
+	 * 計算量：O(n√q)
+	 * @param callable $add_left 左の区間を伸ばしたときの処理
+	 * @param callable $add_right 右の区間を伸ばしたときの処理
+	 * @param callable $erase_left 左の区間を縮めたときの処理
+	 * @param callable $erase_right 右の区間を縮めたときの処理
+	 * @param callable $out クエリ[l,r)の計算結果を出力する処理
+	 * @return void
+	 */
 	private function sub_build(callable $add_left, callable $add_right, callable $erase_left, callable $erase_right, callable $out): void
 	{
 		$q = count($this->lr);
@@ -40,6 +69,14 @@ class Mo
 		}
 	}
 
+	/**
+	 * ビルド
+	 * 計算量：O(n√q)
+	 * @param callable $add 区間を伸ばしたときの処理
+	 * @param callable $erase 区間を縮めたときの処理
+	 * @param callable $out クエリ[l,r)の計算結果を出力する処理
+	 * @return void
+	 */
 	public function build(callable $add, callable $erase, callable $out): void
 	{
 		$this->sub_build($add, $add, $erase, $erase, $out);
