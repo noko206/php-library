cf40bc37ed47db14c0af47a60993f8e744ab717c
noko206 <61347127+noko206@users.noreply.github.com>
2024-02-05 17:15:46 +0000

    docs: 座圧の引数と戻り値のコメントを修正

--- compress.php
+++ compress.php
@@ -3,8 +3,8 @@
 /**
  * 座標圧縮
  *
- * @param list<int> &$a 座標圧縮したい配列
- * @return list<int> 復元用配列
+ * @param list<int> &$a 座標圧縮したい配列(座標圧縮後の配列に上書きされる)
+ * @return list<int> 圧縮用配列
  */
 function compress(array &$a): array
 {
